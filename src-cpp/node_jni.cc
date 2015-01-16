a4feeeaaee79d63f5be2c8b627508b2d8c7cad4d
Jasm Sison <jasm.sison@gmail.com>
2015-01-16 12:29:05 +0100

    last hurdle, convert const char* (jbyte) to char*, so main will accept the 2nd param

--- src-cpp/node_jni.cc
+++ src-cpp/node_jni.cc
@@ -1,32 +1,52 @@
 #include "node.h"
 #include <jni.h>
+#include <string>
+#include <iostream>
 
 #include "node_jni.h"
 
+union charunion {
+  char *chr;
+  const char* cchr;
+};
+
+
+/**
+ * TODO I can't decide on snake, camel case or C-style abbrev.
+ */
 namespace node
 {
   /** I decided to pass a single concatenated jstring instead of an array of strings from java */
-  JNIEXPORT jint JNICALL Java_NodeJNI_start(JNIEnv *env, jobject obj, jint argc, jobjectArray argv) {
+  JNIEXPORT jint JNICALL Java_NodeJNI_start(JNIEnv *env, jobject obj, jint jni_argc, jobjectArray jni_argv) {
 
-    int stringCount = env->GetArrayLength(stringArray);
+    int len = env->GetArrayLength(jni_argv); // should be equal to argc
 
-    for (int i=0; i<stringCount; i++) {
-        jstring string = (jstring) GetObjectArrayElement(env, stringArray, i);
-        const char *rawString = GetStringUTFChars(env, string, 0);
-        // Don't forget to call `ReleaseStringUTFChars` when you're done.
+    const char** argv = new const char*[len];
+    jstring* jstringArr = new jstring[len];
+
+    // machine value type conversion, wow, gotta love high-level abstractions...
+    for (int i=0; i<len; i++) {
+        jstringArr[i] = (jstring) env->GetObjectArrayElement(jni_argv, i);
+        argv[i] = env->GetStringUTFChars(jstringArr[i], 0);
     }
-
-    const jbyte *str;
-    str = (*env)->GetStringUTFChars(env, argv, str);
 
     // capture exit result
-    int returnValue = node::Start((int) argc, argv);
+    int returnValue = node::Start((int) jni_argc, argv);
+    // TODO jint is a typedef for long on an arm 64 and endianness? Phrack it. Just cast.
+    // figure out macros later
 
-    // prevent memory leaks
-    for (int i=0; i<stringCount; i++) {
-      (*env)->ReleaseStringUTFChars(env, argv, str);
+
+    for (int i=0; i<len; i++) {
+      // debug, TODO redirect stdout to android Log
+      std::cout << std::string(argv[i]);
+
+      // prevent memory leaks
+      env->ReleaseStringUTFChars(jstringArr[i], argv[i]);
     }
-
+    
+    // deallocate arrays
+    delete argv;
+    delete jstringArr;
 
     return returnValue;
   }
