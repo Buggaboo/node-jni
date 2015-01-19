2ff6db2dce98f278d0b03ff297402e0e8c90b7b9
Jasm Sison <jasm.sison@gmail.com>
2015-01-19 05:02:15 +0100

    removed failed ndk-build experiment, the node gyp build system is more than capable of building a shared library

--- Application.mk
+++
@@ -1,6 +0,0 @@
-# lifesaver: https://aabdelfattah.wordpress.com/2013/04/08/android-build-system-ultimate-guide/
-APP_STL := gnustl_static # c++_static # stlport_static...
-APP_PLATFORM := android-9
-APP_ABI := armeabi armeabi-v7a # TODO all (soon also: x86, arm64)
-#NDK_TOOLCHAIN_VERSION := 4.8
-NDK_TOOLCHAIN := armeabi-4.8
